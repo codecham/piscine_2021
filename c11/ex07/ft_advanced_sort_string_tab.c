dafe4431f5ab21e462328d6d686640cd08605a81
Corentin Dechamps <corentin@Corentins-MacBook-Pro.local>
2021-12-01 07:54:27 +0100

    c11 modif

--- c11/ex07/ft_advanced_sort_string_tab.c
+++ c11/ex07/ft_advanced_sort_string_tab.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/01 07:03:55 by dcorenti          #+#    #+#             */
-/*   Updated: 2021/12/01 07:45:31 by dcorenti         ###   ########.fr       */
+/*   Created: 2021/12/01 07:46:13 by dcorenti          #+#    #+#             */
+/*   Updated: 2021/12/01 07:54:06 by dcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	return (s1[i] - s2[i]);
-}
 
 void	ft_swap(char **s1, char **s2)
 {
@@ -26,10 +16,10 @@ void	ft_swap(char **s1, char **s2)
 
 	tmp = *s1;
 	*s1 = *s2;
-	*s2 = tmp;
+	*s2 = *tmp;
 }
 
-void	ft_sort_string_tab(char **tab)
+void	ft_advanced_sort_string_tab(char **tab, int(*cmp)(char *, char *))
 {
 	int	size;
 	int	i;
@@ -44,7 +34,7 @@ void	ft_sort_string_tab(char **tab)
 		j = 0;
 		while (j < size - 1)
 		{
-			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			if (cmp(tab[j], tab[j + 1]) > 0)
 				ft_swap(&tab[j], &tab[j + 1]);
 			j++;
 		}
